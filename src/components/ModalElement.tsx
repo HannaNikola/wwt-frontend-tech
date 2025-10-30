80335107c491de69915678070fef32576f51d6e6
HannaNikola <111384616+HannaNikola@users.noreply.github.com>
2025-10-30 15:58:09 +0100

    filter_data

--- src/components/ModalElement.tsx
+++ src/components/ModalElement.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import { useModalStore } from '@/store/modalStore'
 import { FilterForm } from './FilterForm'
+import { X } from 'lucide-react';
 
 
 
@@ -34,7 +35,7 @@ export const ModalElement = () => {
                  ${animate ? 'scale-100 opacity-100' : 'scale-95 opacity-0'}`}
 			>
 				<button  onClick={() => closeModal()} className='absolute top-[54px] right-[35px] '>
-                 <svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" viewBox="0 0 24 24"><path fill="currentColor" d="M6.4 19L5 17.6l5.6-5.6L5 6.4L6.4 5l5.6 5.6L17.6 5L19 6.4L13.4 12l5.6 5.6l-1.4 1.4l-5.6-5.6z"/></svg>					
+                 <X size={24} strokeWidth={2.5} fill='#31393C'/>
 				</button>
 				<FilterForm />
 			</div>
